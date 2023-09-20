6f292558d79de5b734ec6e2563b55ccbe09506cd
James Gill <james@roboticmonsters.com>
2023-09-20 15:13:14 +1000

    make sure page titles are using Typography

--- src/scenes/contact/index.tsx
+++ src/scenes/contact/index.tsx
@@ -2,13 +2,16 @@ import PageWrapper from '~/components/PageWrapper';
 import { RouteProps } from '~/constants/routes/types';
 import helloThere from '~/assets/images/page-heros/hellotherethumb.webp';
 import useStyles from './styles';
+import Typography from '~/components/Typography';
 
 const Contact = ({ routeKey, title }: RouteProps) => {
   const styles = useStyles();
   return (
     <PageWrapper>
       <div>
-        {routeKey} - {title}
+        <Typography as="h1" variant="pageTitle">
+          {title}
+        </Typography>
         <img className={styles.image} src={helloThere} alt="Hello there" />
       </div>
     </PageWrapper>
--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -2,13 +2,16 @@ import PageWrapper from '~/components/PageWrapper';
 import { RouteProps } from '~/constants/routes/types';
 import may from '~/assets/images/page-heros/may.jpeg';
 import useStyles from './styles';
+import Typography from '~/components/Typography';
 
 const Home = ({ routeKey, title }: RouteProps) => {
   const styles = useStyles();
   return (
     <PageWrapper>
       <div>
-        {routeKey} - {title}
+        <Typography as="h1" variant="pageTitle">
+          {title}
+        </Typography>
         <img className={styles.image} src={may} alt="May the 4th be with you" />
       </div>
     </PageWrapper>
